438eaf25e65bf4b34816644409044e93b8afd8f4
Slapstango <stephenm50@yahoo.com>
2025-06-16 12:56:27 -0400

    Create Dashboard.js

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Dashboard</h2>
+      <p>This is your crash data dashboard.</p>
+    </div>
+  );
+}
+
+export default Dashboard;

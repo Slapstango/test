940df14e937056e15a41f4a82a72b6b23db15429
Slapstango <stephenm50@yahoo.com>
2025-06-16 12:23:11 -0400

    Add files via upload

---
+++ FreshCrashPortal/src/pages/MapViewer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MapViewer() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Map Viewer</h2>
+      <div style={{ height: '300px', backgroundColor: '#ccc' }}>
+        Map goes here.
+      </div>
+    </div>
+  );
+}
+
+export default MapViewer;
